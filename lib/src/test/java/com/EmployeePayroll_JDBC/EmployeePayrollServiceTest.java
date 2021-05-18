1ae3888db845ab484ca386e5ac8b3e31d5b81596
mohdarshad1 <mdarshad181@gmail.com>
2021-05-18 17:15:49 +0530

    [Mohd Arshad] : Add Add Employee

--- lib/src/test/java/com/EmployeePayroll_JDBC/EmployeePayrollServiceTest.java
+++ lib/src/test/java/com/EmployeePayroll_JDBC/EmployeePayrollServiceTest.java
@@ -14,7 +14,7 @@ public class EmployeePayrollServiceTest {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		List<EmployeePayrollData> employeePayrollData;
 		employeePayrollData = employeePayrollService.readEmployeePayrollData();
-		Assert.assertEquals(3, employeePayrollData.size());
+		Assert.assertEquals(4, employeePayrollData.size());
 	}
 
 	// UC3
@@ -61,5 +61,14 @@ public class EmployeePayrollServiceTest {
 		Map<String, Double> maxSalaryByGender = employeePayrollService.performOperationByGender("salary", "MAX");
 		Assert.assertEquals(3000000.0, maxSalaryByGender.get("F"), 0.0);
 	}
+	//UC7
+	 @Test
+	    public void givenNewEmployee_WhenAdded_ShouldSyncWithDB() throws EmployeePayrollException {
+	    	EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+	    	employeePayrollService.readEmployeePayrollData();
+	    	employeePayrollService.addEmployeeToPayroll("Mark","M", 5000000.00, LocalDate.now());
+	        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Mark");
+	        Assert.assertTrue(result);
+	    }
 	
 }
